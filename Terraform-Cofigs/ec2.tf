33ae51fa84b90a8e6345456bc51939848fa36ed5
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-05-27 10:00:04 -0400

    Update ec2.tf

--- Terraform-Cofigs/ec2.tf
+++ Terraform-Cofigs/ec2.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "demoinstance" {
 # Creating 2nd EC2 instance in Public Subnet
 resource "aws_instance" "demoinstance1" {
   ami                         = "ami-087c17d1fe0178315"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.small"
   count                       = 1
   key_name                    = "tests"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
